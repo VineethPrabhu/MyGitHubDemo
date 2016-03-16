37f35bac2fe7b05ee60163ed5f441df787c91ec9
Vineeth Prabhu <vineeth.prabhu@gmail.com>
2016-03-17 00:35:03 +0530

    Adding the sample POJO

---
+++ myWebApp/webappdemo/src/main/java/com/vin/sample/webapp/json/MyBean.java
@@ -0,0 +1,25 @@
+package com.vin.sample.webapp.json;
+
+public class MyBean {
+
+	private String name;
+
+	private String address;
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(final String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+}
